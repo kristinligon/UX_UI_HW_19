0df08d16c1b96f4bf626c791453bb8954987f96f
kristinligon <110652169+kristinligon@users.noreply.github.com>
2023-01-09 18:32:01 -0600

    edits 1.9.23

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ console.log("Index index.js file is loaded corrently!")
 
 $(document).ready(function(){
     $(".donatebutton h2").hover(function(){
-      $(this).css("background-color", "black", "border-radius", "5px");
+      $(this).css("background-color", "black");
       }, function(){
-      $(this).css("background-color", "#396A4A", "border-radius", "5px");
+      $(this).css("background-color", "#396A4A");
     }, function(){
         $(this).css("border-radius", "5px");
       }, function(){
@@ -12,9 +12,25 @@ $(document).ready(function(){
     });
   });
 
+  $(document).ready(function(){
+    $(".navbar").hover(function(){
+      $(this).css("background-color", "#396A4A");
+      }, function(){
+      $(this).css("background-color", "#E5F2E6");
+    });
+  });
+
+  $(document).ready(function(){
+    $(".volunteerlinks h3").hover(function(){
+      $(this).css("background-color", "black");
+      }, function(){
+      $(this).css("background-color", "#396A4A");
+    });
+  });
+
 /*$(document).ready(function(){
     $(".volunteertitle").click (function(){
-      $(this).animate({ shake 0.5s });
+      $(this).animate(shake 0.5s);
     });
   });
 
@@ -28,16 +44,8 @@ $(document).ready(function(){
 
 $(".main img").hover(function(){
     $(".main img").slideToggle()
-});*/
+});
 
-$(document).ready(function(){
-    $(".volunteerlinks h3").hover(function(){
-      $(this).css("background-color", "black");
-      }, function(){
-      $(this).css("background-color", "#396A4A");
-    });
-  });
-
-  $(".signupbutton").click(function(){
+/*$(".signupbutton").click(function(){
     $(".signupbutton").css("background-color: black");
-  });
+  });*/
